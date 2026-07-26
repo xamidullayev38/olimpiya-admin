e3e2f82f099b73e7511ec62dde6328665e479da6
xamidullayev38 <xamidullayevsaidkamol@gmail.com>
2026-07-26 18:38:28 +0500

    fix: default API_BASE_URL to AWS server

--- src/shared/api/endpoints.ts
+++ src/shared/api/endpoints.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api/v1";
+  process.env.NEXT_PUBLIC_API_URL || "https://olimpiya.saidly.me/api/v1";
 
 export const ENDPOINTS = {
   AUTH: {
